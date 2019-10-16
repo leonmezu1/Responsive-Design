a6cc8aafd21d8960217148543d7e6b95d3bb243a
leonmezu1 <leo7xs@gmail.com>
2019-10-16 15:46:33 -0500

    Project finished, finally...

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
-window.onresize = function () {
-  toggle();
-};
-
 window.onscroll = function () {
-  myFunction();
+  logosticky();
 };
 
-//resize, scroll
+window.onresize = function () {
+  resizable();
+};
 
 var nav = document.getElementById('navid');
 var sticky = nav.offsetTop;
-var logo = document.getElementById('logo2id');
-var biglogo = document.getElementById('logoid');
+var image = document.getElementById('logo-token');
+var logo = document.getElementById('logo-token-2');
 var mainnav = document.getElementById('mainnavid');
 var yellow  = document.getElementById('yellow');
 
-function myFunction() {
+function logosticky() {
   if (window.pageYOffset > sticky) {
-    biglogo.style.display = 'none';
+    image.style.display = 'none';
     logo.style.display = 'block';
     mainnav.style.paddingLeft = '120px';
     yellow.style.paddingLeft = '126px';
   } else if (window.pageYOffset < sticky && window.innerWidth >= 1023) {
-    biglogo.style.display = 'none';
+    image.style.display = 'none';
     logo.style.display = 'none';
     mainnav.style.paddingLeft = '14px';
     yellow.style.paddingLeft = '20px';
   } else if (window.pageYOffset < sticky && window.innerWidth <= 1024) {
     logo.style.display = 'none';
-    biglogo.style.display = 'block';
+    image.style.display = 'block';
   }
 }
 
-function toggle() {
+function resizable() {
   if (window.innerWidth > 1023) {
-    biglogo.style.display = 'none';
+    image.style.display = 'none';
     logo.style.display = 'none';
     mainnav.style.paddingLeft = '15px';
   } else if (window.innerWidth < 1024) {
     logo.style.display = 'none';
-    biglogo.style.display = 'block';
+    image.style.display = 'block';
     mainnav.style.paddingLeft = '120px';
   }
 }
